e0f8f9234ce584784e16280f1f90e19cdd4a2e14
Matt Hova <matthova87@gmail.com>
2020-10-12 22:26:22 -0700

    added more types

--- Sources/Rendering/Misc/GenericRenderWindow/index.d.ts
+++ Sources/Rendering/Misc/GenericRenderWindow/index.d.ts
@@ -1,11 +1,29 @@
+type Renderer = import('../../Core/Renderer').Renderer;
 export interface T100 {
-  [key: string]: any;
+  background: [number, number, number];
+  listenWindowResize: boolean;
+  container: HTMLElement | null;
 }
-declare function extend_1(publicAPI: any, model: any, initialValues?: T100): void;
+declare function extend_1(
+  publicAPI: any,
+  model: any,
+  initialValues?: Partial<T100>
+): void;
 export const extend: typeof extend_1;
-export const newInstance: any;
+export interface GenericRenderWindow {
+  setBackground: (background: any) => void;
+  resize: () => void;
+  setContainer: (el: HTMLElement) => void;
+  delete: () => void;
+  getRenderWindow: () => any;
+  getRenderer: () => Renderer;
+  getOpenGLRenderWindow: () => any;
+  getInteractor: () => any;
+  getContainer: () => any;
+}
+export const newInstance: (args: Partial<T100>) => GenericRenderWindow;
 export interface T101 {
-  newInstance: any;
+  newInstance: (args: Partial<T00>) => GenericRenderWindow;
   extend: typeof extend_1;
 }
 declare const T102: T101;
